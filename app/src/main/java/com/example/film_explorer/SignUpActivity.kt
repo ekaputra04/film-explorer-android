562c4b28d13d1d1dbcd0596d336c1cafc5e0fa16
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-07-08 23:53:25 +0800

    feat: SignUpActivity [layout]

---
+++ app/src/main/java/com/example/film_explorer/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.film_explorer
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
